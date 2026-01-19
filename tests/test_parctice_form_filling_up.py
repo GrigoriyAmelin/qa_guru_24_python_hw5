dc8f645d16ae5eccbb5ee1bd8d36687f28b8d54e
Grigoriy Amelin <gamelin@ecom.tech>
2026-01-20 02:57:25 +0300

    completed test_practice_form_filling_up

--- tests/test_parctice_form_filling_up.py
+++ tests/test_parctice_form_filling_up.py
@@ -17,7 +17,7 @@ def test_practice_form_filling_up():
     br.element('#lastName').type('Stojanovic')
     br.element('#userEmail').type('mirko-stojanovic@srbijavoz.rs')
     br.element(by.text("Male")).click().should(be.enabled)
-    br.element('#userNumber').type('+0682826586')
+    br.element('#userNumber').type('0682826586')
 
     # Установка даты в календаре
     br.element("#dateOfBirthInput").click()
@@ -42,5 +42,34 @@ def test_practice_form_filling_up():
     abspath = os.path.join(dirname, 'resources', '111.png')
     br.element("#uploadPicture").send_keys(abspath)
 
+    # Выбор адреса
+    br.element('#currentAddress').set_value('11545 Novi Sad, Srbija, ul. Cara Dusana, 9, sprat 2, stan 231')
+    br.element('#state').click()
+    br.element(by.text('Haryana')).click()
+    br.element('#city').click()
+    br.element(by.text('Panipat')).click()
 
+    br.element('#submit').click()
 
+    # Проверка формы
+    br.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    (br.element('.table-responsive').element(by.text('Student Name')).element('..').
+     should(have.text('Mirko Stojanovic')))
+    (br.element('.table-responsive').element(by.text('Student Email')).element('..').
+     should(have.text('mirko-stojanovic@srbijavoz.rs')))
+    (br.element('.table-responsive').element(by.text('Gender')).element('..').
+     should(have.text('Male')))
+    (br.element('.table-responsive').element(by.text('Mobile')).element('..').
+     should(have.text('0682826586')))
+    (br.element('.table-responsive').element(by.text('Date of Birth')).element('..').
+     should(have.text('08 October,1979')))
+    (br.element('.table-responsive').element(by.text('Subjects')).element('..').
+     should(have.text('English, Commerce, Arts')))
+    (br.element('.table-responsive').element(by.text('Hobbies')).element('..').
+     should(have.text('Reading')))
+    (br.element('.table-responsive').element(by.text('Picture')).element('..').
+     should(have.text('111.png')))
+    (br.element('.table-responsive').element(by.text('Address')).element('..').
+     should(have.text('11545 Novi Sad, Srbija, ul. Cara Dusana, 9, sprat 2, stan 231')))
+    (br.element('.table-responsive').element(by.text('State and City')).element('..').
+     should(have.text('Haryana Panipat')))
